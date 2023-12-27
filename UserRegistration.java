90d282f17f001d7fb083e4b3484dc535c4c7e669
Uday <uday@gmail.com>
2023-12-27 19:20:06 +0530

    Added last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -12,19 +12,30 @@ public class UserRegistration {
         System.out.print("Enter First Name: ");
         String firstName = scanner.nextLine();
 
-        // Validate and display the result
-        if (isValidFirstName(firstName)) {
+        // Validate and display the result for First Name
+        if (isValidName(firstName)) {
             System.out.println("Valid First Name: " + firstName);
         } else {
             System.out.println("Invalid First Name. Please follow the specified criteria.");
         }
 
+        // Get user input for Last Name
+        System.out.print("Enter Last Name: ");
+        String lastName = scanner.nextLine();
+
+        // Validate and display the result for Last Name
+        if (isValidName(lastName)) {
+            System.out.println("Valid Last Name: " + lastName);
+        } else {
+            System.out.println("Invalid Last Name. Please follow the specified criteria.");
+        }
+
         scanner.close();
     }
 
-    // Function to validate the First Name
-    private static boolean isValidFirstName(String firstName) {
-        // Check if the First Name starts with a capital letter and has a minimum length of 3 characters
-        return firstName.matches("[A-Z][a-zA-Z]{2,}");
+    // Function to validate the First and Last Name
+    private static boolean isValidName(String name) {
+        // Check if the Name starts with a capital letter and has a minimum length of 3 characters
+        return name.matches("[A-Z][a-zA-Z]{2,}");
     }
 }
